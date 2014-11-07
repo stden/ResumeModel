52c79f134b73ef35a892d6fc75c29acb69d18fff
Denis Stepulenok <super.denis@gmail.com>
2014-11-07 22:24:55 +0400

    Добавил класс Resume

---
+++ src/test/java/ResumeTest.java
@@ -0,0 +1,13 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ */
+public class ResumeTest extends Assert {
+    @Test
+    public void test1() {
+        Resume resume = new Resume();
+        assertEquals(4, 2 * 2);
+    }
+}
